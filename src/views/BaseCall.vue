a79c79dcd03184dd747604d07db70e51c69c69bb
Wonyeong Choi <coma01155@gmail.com>
2021-05-21 16:59:35 +0900

    Add call component

--- src/views/BaseCall.vue
+++ src/views/BaseCall.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>BaseDashboard</p>
+  <v-container fill-height>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <router-view />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+
 export default {
-  name: 'BaseDashboard',
+  name: 'BaseCall',
 };
 </script>
--- src/views/call/CallDefault.vue
+++ src/views/call/CallDefault.vue
@@ -1,9 +1,27 @@
 <template>
-  <p>CallDefault</p>
+  <v-container>
+    <v-row>
+      <v-col cols="auto">
+        <div>
+          지도
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="auto"
+      >
+        <call-card />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import CallCard from '@/components/call/CallCard.vue';
+
 export default {
   name: 'CallDefault',
+  components: { CallCard },
 };
 </script>

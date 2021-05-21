65601d1b3188a77452e2fb1b6c52528fc3c72602
Hyunmin Jung <jung@hyunmin.dev>
2021-05-21 15:22:07 +0900

    Add AuthSignIn component

--- src/views/auth/AuthSignIn.vue
+++ src/views/auth/AuthSignIn.vue
@@ -1,9 +1,15 @@
 <template>
-  <p>AuthSignIn</p>
+  <sign-in-card />
 </template>
 
 <script>
+import SignInCard from '@/components/auth/sign-in/SignInCard.vue';
+
 export default {
   name: 'AuthSignIn',
+
+  components: {
+    SignInCard,
+  },
 };
 </script>

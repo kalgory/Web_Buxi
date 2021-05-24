8b62044d1d89b2dc839fd5a29dac8044eae77c3a
Dongmin Kim <ehdals0211@naver.com>
2021-05-25 00:29:51 +0900

    rollback history

--- src/components/naver/NaverMap.vue
+++ src/components/naver/NaverMap.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="map" />
+  <div id="map">
+  </div>
 </template>
 
 <script>
+
 export default {
   name: 'NaverMap',
 
@@ -25,21 +27,23 @@ export default {
   data: () => ({
     map: null,
     markers: [],
+    test: 0,
     hasClickEvent: true,
     isClickLoading: false,
   }),
 
   mounted() {
     // eslint-disable-next-line no-unused-expressions
-    window.naver ? this.initMap() : this.addNaverMapApiScript();
+    window.naver ? this.initMap() : this.naverMapApiScript();
+    window.clickInfoWindowButton = this.clickInfoWindowButton;
   },
 
   methods: {
-    addNaverMapApiScript() {
+    naverMapApiScript() {
       /* global naver */
       const script = document.createElement('script');
       const type = 'text/javascript';
-      const src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${this.$clientID}`;
+      const src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=s4ygn6mjbi';
       script.setAttribute('async', '');
       script.setAttribute('defer', '');
       script.setAttribute('type', type);
@@ -55,28 +59,55 @@ export default {
         center: new naver.maps.LatLng(this.centerPosition.lat, this.centerPosition.lng),
         zoom: this.zoom,
       });
-      this.$emit('mapLoaded');
-      this.addClickMapEventListener();
-      this.addDragendEventListener();
+      this.$emit('load');
+      this.clickEventListener();
+      this.dragEndEventListener();
     },
 
-    addDragendEventListener() {
+    dragEndEventListener() {
       window.naver.maps.Event.addListener(this.map, 'dragend', () => {
+        this.test += 1;
         this.$emit('dragend', {
           lat: this.map.getCenter().y,
           lng: this.map.getCenter().x,
         });
+        this.setMarker({
+          lat: this.map.getCenter().y,
+          lng: this.map.getCenter().x,
+          stop_nm: this.test,
+        });
       });
     },
 
-    addClickMapEventListener() {
-      window.naver.maps.Event.addListener(this.map, 'click', (eventArgument) => {
+    clickEventListener() {
+      window.naver.maps.Event.addListener(this.map, 'click', (position) => {
         if (this.hasClickEvent) {
           this.isClickLoading = true;
-          this.$emit('click', eventArgument.coord);
+          this.$emit('click', position.coord);
           this.hasClickEvent = false;
-          this.$emit('clickMapEventComplete');
+          this.$emit('clickLoading');
           this.isClickLoading = false;
+        }
+      });
+    },
+
+    clickMarkerEventListener(marker, option) {
+      const contentString = [
+        '<div class="iw_inner" style="width: 200px">',
+        '   <h3 class="text-center">', option.stop_nm, '</h1>',
+        '   <p class="center">',
+        `       <button class="button" onclick="clickInfoWindowButton(1, '${encodeURIComponent(JSON.stringify(option))}')">출발지 선택</button>`,
+        `       <button class="button" onclick="clickInfoWindowButton(0, '${encodeURIComponent(JSON.stringify(option))}')">도착지 선택</button>`,
+        '   </p>',
+        '</div>'].join('');
+      const infoWindow = new naver.maps.InfoWindow({
+        content: contentString,
+      });
+      window.naver.maps.Event.addListener(marker, 'click', () => {
+        if (infoWindow.getMap()) {
+          infoWindow.close();
+        } else {
+          infoWindow.open(this.map, marker);
         }
       });
     },
@@ -85,36 +116,58 @@ export default {
       if (!this.isClickLoading) {
         this.hasClickEvent = true;
       } else {
-        this.$on('clickMapEventComplete', () => {
+        this.$on('clickLoading', () => {
           this.hasClickEvent = true;
         });
       }
     },
 
-    addMarker(stationInformation) {
+    initMarker(option) {
       const marker = new window.naver.maps.Marker({
         map: this.map,
-        position: new window.naver.maps.LatLng(stationInformation.lat, stationInformation.lng),
+        position: new window.naver.maps.LatLng(option.lat, option.lng),
         clickable: true,
       });
       marker.setMap(this.map);
       this.markers.push(marker);
-      window.naver.maps.Event.addListener(marker, 'click', () => {
-        this.$emit('clickMarker', stationInformation);
-      });
+      this.clickMarkerEventListener(marker, option);
     },
 
-    setMarker(stationInformation) {
+    setMarker(option) {
       // eslint-disable-next-line no-unused-expressions
-      window.naver ? this.addMarker(stationInformation) : this.$on('mapLoaded', () => this.addMarker());
+      window.naver ? this.initMarker(option) : this.$on('load', () => this.initMarker(option));
     },
 
     removeMarkers() {
       this.markers.forEach((marker) => {
         marker.setMap(null);
       });
+      this.markers = null;
       this.setHasClickEvent();
+    },
+
+    clickInfoWindowButton(select, option) {
+      const object = JSON.parse(decodeURIComponent(option));
+      if (select === 1) {
+        this.$emit('startStation', object);
+      } else if (select === 0) {
+        this.$emit('endStation', object);
+      }
     },
   },
 };
 </script>
+
+<style>
+.button {
+  width: 80px;
+  height: 30px;
+  background-color: #fff;
+  border: 3px solid #ebebeb;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+}
+</style>

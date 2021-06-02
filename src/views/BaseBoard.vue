76a3ff895da67d6f9132bc97ef18ac7ed268e70c
Hyunmin Jung <jung@hyunmin.dev>
2021-06-02 18:08:27 +0900

    Modify file structure

---
+++ src/views/BaseBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'BaseBoard',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/board/BoardBefore.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'BoardBefore.vue',
+};
+</script>
+
+<style scoped>
+
+</style>
